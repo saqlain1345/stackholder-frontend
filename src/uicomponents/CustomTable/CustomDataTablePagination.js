a1ab15089ca18fd524fa2873d93033e52d5e02a9
Abdullah borkala <abdullahborkala@gmail.com>
2022-09-28 14:48:46 +0530

    sidebar bug fixed, pagination added to table

--- src/uicomponents/CustomTable/CustomDataTablePagination.js
+++ src/uicomponents/CustomTable/CustomDataTablePagination.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CustomDataTablePagination.css'
 
 const CustomDataTablePagination = () => {
   let n = 8;
+  const [showPageNoSubmit, setShowPageNoSubmit] = useState(false)
+  const handleChange = (e)=>{
+    if(e.target.value === "") setShowPageNoSubmit(false)
+    else setShowPageNoSubmit(true) 
+  }
   return (
     <div className='tablePaginationContainer'>
       <div className='pageContents'>
-      {Array.apply(null, {length:20}).map((e,i)=>(
           <div className='pagesNumber'>
-            <p>{i}</p>
+            <p>&lt;</p>
           </div>
-        ))
-      }
+          <div className='paginationInput'>
+            <input type="number" placeholder='Page No' onChange={handleChange}/>
+          </div>
+          <div className='paginationPageSubmitButton' style={showPageNoSubmit ? {display: "block", opacity: 1,  animation: "fade 1s", visibility: "visible", transition: "all 0.5s"} : {display: "none", opacity: 0, visibility: "hidden", transition: "all 0.5s"}}>
+            <img src='/img/submit.png' />
+          </div>
+          <div className='pagesNumber'>
+            <p>&gt;</p>
+          </div>
       </div>
     </div>
   )
